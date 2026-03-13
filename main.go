1cdc76637f9a19974974770a40607a1e823e55a7
DilLemon <131652829+DilLemon@users.noreply.github.com>
2026-03-14 00:00:40 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 var clients = make(map[*websocket.Conn]bool)
 var broadcast = make(chan string)
