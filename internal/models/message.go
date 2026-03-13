b552c056a6ee10218577d92713aecffb1cbf378e
DilLemon <131652829+DilLemon@users.noreply.github.com>
2026-03-14 00:37:09 +0300

    Update message.go

--- internal/models/message.go
+++ internal/models/message.go
@@ -1,1 +1,15 @@
+package models
 
+type Message struct {
+	Type   string `json:"type"`
+	From   string `json:"from"`
+	To     string `json:"to"`
+	Text   string `json:"text"`
+	Time   int64  `json:"time"`
+	Status string `json:"status"`
+}
+
+type Login struct {
+	Type string `json:"type"`
+	User string `json:"user"`
+}
